1eb12385159f64b0fcf580b2f0e563733786cce0
Ria Gupta <ria0619.becse24@chitkara.edu.in>
2026-03-11 09:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (80.73%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -10,30 +10,51 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        List<Integer> arr = new ArrayList<>();
-        ListNode dummy = head;
-        while(dummy!=null){
-            arr.add(dummy.val);
-            dummy = dummy.next;
-        }
-        List<Integer> arr2 = new ArrayList<>();
-        for(int i = 0;i<arr.size();i++){
-            if(arr.get(i) < x){
-                arr2.add(arr.get(i));
+        // List<Integer> arr = new ArrayList<>();
+        // ListNode dummy = head;
+        // while(dummy!=null){
+        //     arr.add(dummy.val);
+        //     dummy = dummy.next;
+        // }
+        // List<Integer> arr2 = new ArrayList<>();
+        // for(int i = 0;i<arr.size();i++){
+        //     if(arr.get(i) < x){
+        //         arr2.add(arr.get(i));
+        //     }
+        // }
+        // for(int i = 0;i<arr.size();i++){
+        //     if(arr.get(i) >= x){
+        //         arr2.add(arr.get(i));
+        //     }
+        // }
+        // ListNode curr = new ListNode(0);
+        // ListNode top = curr;
+        // for(int i = 0;i<arr.size();i++){
+        //     ListNode newNode = new ListNode(arr2.get(i));
+        //     top.next = newNode;
+        //     top = top.next;
+        // }
+        // return curr.next;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        ListNode h1 = head;
+        while(h1!=null){
+            if(h1.val < x){
+                ListNode newNode = new ListNode(h1.val);
+                curr.next = newNode;
+                curr = curr.next;
             }
+            h1 = h1.next;
         }
-        for(int i = 0;i<arr.size();i++){
-            if(arr.get(i) >= x){
-                arr2.add(arr.get(i));
+        h1 = head;
+        while(h1!=null){
+            if(h1.val >= x){
+                ListNode newNode = new ListNode(h1.val);
+                curr.next = newNode;
+                curr = curr.next;
             }
+            h1 = h1.next;
         }
-        ListNode curr = new ListNode(0);
-        ListNode top = curr;
-        for(int i = 0;i<arr.size();i++){
-            ListNode newNode = new ListNode(arr2.get(i));
-            top.next = newNode;
-            top = top.next;
-        }
-        return curr.next;
+        return dummy.next;
     }
 }
