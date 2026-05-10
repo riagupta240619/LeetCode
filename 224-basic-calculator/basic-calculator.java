b33cffbb12c3506db092e290e76eaa8cdb2fa387
Ria Gupta <ria0619.becse24@chitkara.edu.in>
2026-05-10 12:14:23 +0530

    Time: 10 ms (84.96%) | Memory: 46.5 MB (54.03%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,54 +1,38 @@
 import java.util.*;
-
 class Solution {
-
     public int calculate(String s) {
-
         Stack<Integer> stack = new Stack<>();
-
         int num = 0;
         int result = 0;
-        int sign = 1;   // +1 or -1
-
+        int sign = 1;   
         for (int i = 0; i < s.length(); i++) {
-
             char ch = s.charAt(i);
-
-            // build number
             if (Character.isDigit(ch)) {
                 num = num * 10 + (ch - '0');
             }
-
             else if (ch == '+') {
                 result += sign * num;
                 num = 0;
                 sign = 1;
             }
-
             else if (ch == '-') {
                 result += sign * num;
                 num = 0;
                 sign = -1;
             }
-
             else if (ch == '(') {
-                // save current state
                 stack.push(result);
                 stack.push(sign);
-
                 result = 0;
                 sign = 1;
             }
-
             else if (ch == ')') {
                 result += sign * num;
                 num = 0;
-
-                result *= stack.pop(); // sign before bracket
-                result += stack.pop(); // result before bracket
+                result *= stack.pop(); 
+                result += stack.pop(); 
             }
         }
-
         return result + sign * num;
     }
 }
