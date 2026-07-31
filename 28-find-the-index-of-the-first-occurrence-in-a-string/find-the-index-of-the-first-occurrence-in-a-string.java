00f96aafccd4f4265fa5023269c9f165286a4505
Ria Gupta <ria0619.becse24@chitkara.edu.in>
2026-07-31 09:52:57 +0530

    Time: 3 ms (8.89%) | Memory: 43.2 MB (33.93%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,10 +1,62 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        for(int i = 0;i<=n-m;i++){
-            if(haystack.substring(i,i+m).equals(needle)) return i;
+    static void lps(String pat, int[] lps){
+        int len = 0;
+        lps[0] = 0;
+        int m = pat.length();
+        int i = 1;
+        while(i<m){
+            if(pat.charAt(i) == pat.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len!=0){
+                    len = lps[len-1];
+                }else{
+                    lps[len] = len;
+                    i++;
+                }
+            }
+        }
+    }
+    static int search(String txt, String pat){
+        int n = txt.length();
+        int m = pat.length();
+        int res = 0;
+        int[] lps = new int[m];
+        lps(pat, lps);
+        int i = 0;
+        int j = 0;
+        while(i<n){
+            if(txt.charAt(i) == pat.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j == m){
+                res = i-j;
+                j = lps[j-1];
+                return res;
+            }
+            else if(i<n && txt.charAt(i)!=pat.charAt(j)){
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
         }
         return -1;
+    }
+    public int strStr(String haystack, String needle) {
+        // int n = haystack.length();
+        // int m = needle.length();
+        // for(int i = 0;i<=n-m;i++){
+        //     if(haystack.substring(i,i+m).equals(needle)) return i;
+        // }
+        // return -1;
+
+        //KMP algorithm
+        int res = search(haystack, needle);
+        return res;
     }
 }
